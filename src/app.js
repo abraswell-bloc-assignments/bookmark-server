ef4a9289547307004ad3d2c50a05f2ec7e86b6aa
AmyBraswell <braswellamy@mac.com>
2020-04-14 13:51:57 -0500

    created seperate bearer token component

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
-const logger = require('./logger')
+const validateBearerToken = require('./validate-bearer-token')
 const errorHandler = require('./error-handler')
 const bookmarkRouter = require('./bookmarks/bookmark-router')
 
@@ -21,20 +21,8 @@ app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
 
+app.use(validateBearerToken)
 
-//validate API key
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`)
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  // move to the next middleware 
-  // app won't move on unless you tell it to
-  next()
-})
 
 // router handles endpoints
 app.use(bookmarkRouter)
--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -1,3 +1,19 @@
 require ('dotenv').config()
 const { API_TOKEN } = require('./config')
 const logger = require('./logger')
+
+//validate API key
+function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      logger.error(`Unauthorized request to path: ${req.path}`)
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware 
+    // app won't move on unless you tell it to
+    next()
+  }
+
+  module.exports = validateBearerToken
